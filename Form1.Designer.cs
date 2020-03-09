215df4c2a6501778e89aa2f87e8d007a67229861
FerJoTello <53987518+FerJoTello@users.noreply.github.com>
2020-03-08 22:12:17 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace _OLC1_Proyecto1_201800714
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirArchivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarArchivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.agregarPestañaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.herramientasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cargarThomsonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarTokensToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarErroresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.erroresLéxicosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.herramientasToolStripMenuItem,
+            this.reportesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1434, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirArchivoToolStripMenuItem,
+            this.guardarArchivoToolStripMenuItem,
+            this.agregarPestañaToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(88, 29);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirArchivoToolStripMenuItem
+            // 
+            this.abrirArchivoToolStripMenuItem.Name = "abrirArchivoToolStripMenuItem";
+            this.abrirArchivoToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.abrirArchivoToolStripMenuItem.Text = "Abrir Archivo";
+            this.abrirArchivoToolStripMenuItem.Click += new System.EventHandler(this.AbrirArchivoToolStripMenuItem_Click);
+            // 
+            // guardarArchivoToolStripMenuItem
+            // 
+            this.guardarArchivoToolStripMenuItem.Name = "guardarArchivoToolStripMenuItem";
+            this.guardarArchivoToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.guardarArchivoToolStripMenuItem.Text = "Guardar Archivo";
+            // 
+            // agregarPestañaToolStripMenuItem
+            // 
+            this.agregarPestañaToolStripMenuItem.Name = "agregarPestañaToolStripMenuItem";
+            this.agregarPestañaToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.agregarPestañaToolStripMenuItem.Text = "Agregar Pestaña";
+            // 
+            // herramientasToolStripMenuItem
+            // 
+            this.herramientasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cargarThomsonToolStripMenuItem,
+            this.guardarTokensToolStripMenuItem,
+            this.guardarErroresToolStripMenuItem});
+            this.herramientasToolStripMenuItem.Name = "herramientasToolStripMenuItem";
+            this.herramientasToolStripMenuItem.Size = new System.Drawing.Size(133, 29);
+            this.herramientasToolStripMenuItem.Text = "Herramientas";
+            // 
+            // cargarThomsonToolStripMenuItem
+            // 
+            this.cargarThomsonToolStripMenuItem.Name = "cargarThomsonToolStripMenuItem";
+            this.cargarThomsonToolStripMenuItem.Size = new System.Drawing.Size(246, 34);
+            this.cargarThomsonToolStripMenuItem.Text = "Cargar Thomson";
+            // 
+            // guardarTokensToolStripMenuItem
+            // 
+            this.guardarTokensToolStripMenuItem.Name = "guardarTokensToolStripMenuItem";
+            this.guardarTokensToolStripMenuItem.Size = new System.Drawing.Size(246, 34);
+            this.guardarTokensToolStripMenuItem.Text = "Guardar Tokens";
+            // 
+            // guardarErroresToolStripMenuItem
+            // 
+            this.guardarErroresToolStripMenuItem.Name = "guardarErroresToolStripMenuItem";
+            this.guardarErroresToolStripMenuItem.Size = new System.Drawing.Size(246, 34);
+            this.guardarErroresToolStripMenuItem.Text = "Guardar Errores";
+            // 
+            // reportesToolStripMenuItem
+            // 
+            this.reportesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.erroresLéxicosToolStripMenuItem});
+            this.reportesToolStripMenuItem.Name = "reportesToolStripMenuItem";
+            this.reportesToolStripMenuItem.Size = new System.Drawing.Size(98, 29);
+            this.reportesToolStripMenuItem.Text = "Reportes";
+            // 
+            // erroresLéxicosToolStripMenuItem
+            // 
+            this.erroresLéxicosToolStripMenuItem.Name = "erroresLéxicosToolStripMenuItem";
+            this.erroresLéxicosToolStripMenuItem.Size = new System.Drawing.Size(226, 34);
+            this.erroresLéxicosToolStripMenuItem.Text = "Errores léxicos";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(12, 36);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(743, 660);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(735, 627);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Pestaña 1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(16, 731);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(735, 96);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 708);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Consola";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1434, 839);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MaximumSize = new System.Drawing.Size(1456, 895);
+            this.MinimumSize = new System.Drawing.Size(1456, 895);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirArchivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarArchivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agregarPestañaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem herramientasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cargarThomsonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarTokensToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarErroresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem erroresLéxicosToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
